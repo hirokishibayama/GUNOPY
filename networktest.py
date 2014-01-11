a49550deee5acf527d8fe60dab960a20ff67530b
hirokishibayama <petocobich@gmail.com>
2014-01-11 21:14:31 +0900

    test用

---
+++ networktest.py
@@ -0,0 +1,16 @@
+from bottle import Bottle, run
+
+app = Bottle()
+
+
+@app.route('/')
+def index():
+    return 'Service Running'
+
+run(
+    app,                    # Run |app| Bottle() instance
+    host='0.0.0.0',
+    port=8080,
+    reloader=True,        # restarts the server every time edit a module file
+    debug=True         # Comment out it before deploy
+)
